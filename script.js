0922b797b244b84151fc7f502083cc189a02b3be
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-29 15:09:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const form=document.getElementById('form1')
+const fname=document.getElementsByName('fname')
+const lname=document.getElementsByName('lname')
 function getFormvalue() {
     //Write your code here
-
+	alert(`${fname[0].value} ${lname[0].value}`)
 }
